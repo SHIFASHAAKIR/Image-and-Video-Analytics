54659fcc9684000114348033a3b17f959fc52098
SHIFASHAAKIR <shifashaakir@gmail.com>
2026-06-13 21:08:14 +0530

    Update sampling_quantization.py

--- sampling_quantization.py
+++ sampling_quantization.py
@@ -1,2 +1,7 @@
-print("Digital Image Representation")
+from PIL import Image
+img = Image.open("myimage.jpg")
+sampled = img.resize((img.width//2, img.height//2))
+sampled.save("sampled.jpg")
+quantized = img.quantize(colors=16)
+quantized.save("quantized.jpg")
 print("Sampling and Quantization completed")
